825cd1a56677c461c3b09216216e747e302d46c5
kosm1n <cc_cosmin@hotmail.com>
2022-04-08 12:00:58 +0200

    H2 & Postgres configurations

--- src/main/java/com/example/demo/model/Demo.java
+++ src/main/java/com/example/demo/model/Demo.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "demo", schema = "demo")
+@Table(name = "demo")
 @Builder
 @EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
